9f2135a0263111be6370d6c657101c1fb40d9309
Emerson Joaquim Tito Albino <ealbino@c2r9s3.42luanda.com>
2025-12-02 09:56:43 +0100

    refactor(Program.cs): use suggestion from rider IDE

--- Program.cs
+++ Program.cs
@@ -1,81 +1,82 @@
-﻿namespace SmartCalculator
+﻿using static System.Console;
+
+namespace SmartCalculator
 {
 	class Program
 	{
-		private static int Atoi(string s)
+		private static int Atoi(string? s)
 		{
-			return (int.TryParse(s, out int n) ? int.Parse(s) : 0);
+			return (int.TryParse(s, out var n) ? int.Parse(s) : 0);
 		}
 
 		private static void ShowOptions()
 		{
-			Console.WriteLine("======Operations======");
-			Console.WriteLine("Addition (+):        1");
-			Console.WriteLine("Subtraction (-):     2");
-			Console.WriteLine("Multiplication (x):  3");
-			Console.WriteLine("Division (/):        4");
-			Console.WriteLine("Stop:                5");
-			Console.WriteLine("======================");
+			WriteLine("======Operations======");
+			WriteLine("Addition (+):        1");
+			WriteLine("Subtraction (-):     2");
+			WriteLine("Multiplication (x):  3");
+			WriteLine("Division (/):        4");
+			WriteLine("Stop:                5");
+			WriteLine("======================");
 		}
 
 		private static void AuxCalculate(int option)
 		{
-			int n1, n2;
-			Operations math = new Operations();
+			var math = new Operations();
 
-			Console.Write("insert first value: ");
-			n1 = Atoi(Console.ReadLine());
-			Console.Write("insert second value: ");
-			n2 = Atoi(Console.ReadLine());
-			Console.Clear();
+			Write("insert first value: ");
+			var n1 = Atoi(ReadLine());
+			Write("insert second value: ");
+			var n2 = Atoi(ReadLine());
+			Clear();
 			if (option == 1)
-				Console.WriteLine($"{n1} + {n2} = {math.Add(n1, n2)}");
+				WriteLine($"{n1} + {n2} = {math.Add(n1, n2)}");
 			else if (option == 2)
-				Console.WriteLine($"{n1} - {n2} = {math.Sub(n1, n2)}");
+				WriteLine($"{n1} - {n2} = {math.Sub(n1, n2)}");
 			else if (option == 3)
-				Console.WriteLine($"{n1} x {n2} = {math.Mul(n1, n2)}");
+				WriteLine($"{n1} x {n2} = {math.Mul(n1, n2)}");
 			else
-				Console.WriteLine($"{n1} / {n2} = {math.Div(n1, n2)}");
+				WriteLine($"{n1} / {n2} = {math.Div(n1, n2)}");
 		}
 	
 		private static void Calculate()
 		{
 			int option;
 
-			Console.WriteLine("warning: if not pass a valid integer number, it will be treated as zero (0).");
+			WriteLine("warning: if not pass a valid integer number, it will be treated as zero (0).");
 			do{
 				ShowOptions();
-				Console.Write("insert option:  ");
-				option = Atoi(Console.ReadLine());
+				Write("insert option:  ");
+				option = Atoi(ReadLine());
 				switch (option)
 				{
 					case 1:
-						Console.Clear();
-						Console.WriteLine("You chose Addition (+)");
+						Clear();
+						WriteLine("You chose Addition (+)");
 						AuxCalculate(option);
 						break;
 					case 2:
-						Console.Clear();
-						Console.WriteLine("You chose Subtraction (-)");
+						Clear();
+						WriteLine("You chose Subtraction (-)");
 						AuxCalculate(option);
 						break;
 					case 3:
-						Console.Clear();
-						Console.WriteLine("You chose Multiplication (x)");
+						Clear();
+						WriteLine("You chose Multiplication (x)");
 						AuxCalculate(option);
 						break;
 					case 4:
-						Console.Clear();
-						Console.WriteLine("You chose Division (/)");
+						Clear();
+						WriteLine("You chose Division (/)");
 						AuxCalculate(option);
 						break;
 					case 5:
-						Console.Clear();
-						Console.WriteLine("Program Finished");
+						Clear();
+						WriteLine("Program Finished");
 						break;
 					default:
-						Console.Clear();
-						Console.WriteLine("Invalid Option");
+						Clear();
+						WriteLine("Invalid Option");
 						break;
 				}
 			}while (option != 5);
